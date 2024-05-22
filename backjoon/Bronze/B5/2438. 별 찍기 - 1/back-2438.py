371c7881dac17ca08964ea7f7cf3bc5a0547a38a
milkbottle0305 <bigtog0602@gmail.com>
2024-05-22 16:04:50 +0900

    Delete: useless file

--- backjoon/Bronze/B5/2438. 별 찍기 - 1/back-2438.py
+++
@@ -1,9 +0,0 @@
-def solution(N):
-    for i in range(N):
-        for j in range(i + 1):
-            print('*', end='')
-        print()
-        
-if __name__ == "__main__":
-    N = int(input())
-    solution(N)
