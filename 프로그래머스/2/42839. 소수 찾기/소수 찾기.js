7ddd36c17ab691ae98da15f81e4c81fed94f4a0d
Byeongwoo Yu <bigtog0602@gmail.com>
2024-06-10 15:20:09 +0900

    [level 2] Title: 소수 찾기, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,39 +1,35 @@
 function solution(numbers) {
-    var answer = 0;
+    let answer;
+    let nums = numbers.split(''); 
+    
     const primeSet = new Set();
-    const numberList = numbers.split("")
-    let candidateList = [];
-    for(let i =1;i<numbers.length+1;i++){
-        candidateList = candidateList.concat(permutation(numberList,i));
+    
+    const isPrimeNum = (num) => {
+         if (num <= 1) return false;
+         if (num === 2) return true;
+         for (let i = 2; i <= Math.sqrt(num); i++) {
+              if (num % i === 0) return false;
+         }
+         return true;
     }
-    const joinList = candidateList.map((candidate) => parseInt(candidate.join("")));
-    console.log(joinList);
-    joinList.forEach((join) => {if(isPrime(join)) primeSet.add(join);})
     
-    answer = primeSet.size;
+    const getPermutation = (arr, fixed) => {
+        if(arr.length >= 1) {
+            for (let i=0; i<arr.length; i++) {
+                const newFixed = fixed + arr[i];
+                const copyArr = arr.slice();
+                copyArr.splice(i, 1);
+                
+                if (!primeSet.has(parseInt(newFixed)) && isPrimeNum(parseInt(newFixed))){
+                    primeSet.add(parseInt(newFixed))
+                }
+                getPermutation(copyArr, newFixed);
+            }
+        }
+    }
     
+    getPermutation(nums, '');
+     
+    answer = primeSet.size
     return answer;
-}
-
-function permutation(arr, num){
-  const res = [];
-  if(num === 1) return arr.map((v) => [v]);
-
-  arr.forEach((v, idx, arr) => {
-    const rest = [...arr.slice(0,idx), ...arr.slice(idx+1)];
-    const permutations = permutation(rest, num-1);
-    const attach = permutations.map((permutation) => [v, ...permutation]);
-    res.push(...attach);
-  })
-  return res;
-}
-
-
-
-function isPrime(num) {
-	if(num === 0 || num === 1) return false; 
- 	for(let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
-  		if(num % i === 0) return false;
-	} 
-    return true; 
 }
