3b593bb081eb80de3d5cef365907fc0ef9c8e1f2
Byeongwoo Yu <bigtog0602@gmail.com>
2024-08-14 14:48:26 +0900

    [Gold V] Title: 입국심사, Time: 1076 ms, Memory: 37536 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+function solution(N, M, datas) {
+  let start = 1n;
+  let end = BigInt(M * Math.min(...datas));
+  while (start < end) {
+    let mid = BigInt((start + end) / 2n);
+    let temp = 0n;
+    datas.forEach((v) => {
+      temp += mid / BigInt(v);
+    });
+    if (temp >= M) {
+      end = mid;
+    } else {
+      start = mid + 1n;
+    }
+  }
+  return String(end);
+}
+
+const [N, M] = input[0].split(" ").map((v) => parseInt(v));
+const datas = input.slice(1).map((v) => parseInt(v));
+datas.sort((a, b) => a - b);
+console.log(solution(N, M, datas));
